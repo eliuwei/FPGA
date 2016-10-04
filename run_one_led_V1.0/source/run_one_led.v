92878e991ddfc04cc059c60cf276245ff82475cc
eliuwei <eliuwei@outlook.com>
2016-10-04 19:47:37 +0800

    run_one_led_V1.0

---
+++ run_one_led_V1.0/source/run_one_led.v
@@ -0,0 +1,24 @@
+module run_one_led( CLOCK,  LED0);
+	input CLOCK;
+	output LED0;
+	//reg clk_1Hz;
+	reg LED0;
+	integer counter;
+		initial 
+		begin
+			counter = 0;
+			LED0 = 0;
+		end
+			
+	always @(negedge CLOCK)
+		begin
+			if( counter >= 50000000/2)
+				begin
+					LED0 <= !LED0;
+					counter = 0;
+				end
+			else 
+				counter <= (counter+1);			
+		end
+		
+endmodule
---
+++ run_one_led_V1.0/tcl/run_one_led.tcl
@@ -0,0 +1,25 @@
+#RESET pin
+set_location_assignment	PIN_E15	-to RESET
+
+#CLOCK pin 50M
+set_location_assignment	PIN_E1	-to CLOCK 
+
+
+
+
+#LED pins
+set_location_assignment	PIN_G15	-to LED0
+#set_location_assignment	PIN_F16	-to LED[1]
+#set_location_assignment	PIN_F15	-to LED[2]
+#set_location_assignment	PIN_D16	-to LED[3]
+
+#KEY pins
+#set_location_assignment	PIN_E15	 -to KEY1
+#set_location_assignment	PIN_E16	 -to KEY2
+#set_location_assignment	PIN_M16  -to KEY3
+#set_location_assignment	PIN_M15  -to KEY4
+
+
+
+
+
