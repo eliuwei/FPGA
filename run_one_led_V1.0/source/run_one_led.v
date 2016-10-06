0a32d8ac863f3f0d019369a8b894f96a7f4915ef
柳伟 <eliuwei@outlook.com>
2016-10-06 10:08:23 +0800

    Update run_one_led.v

--- run_one_led_V1.0/source/run_one_led.v
+++ run_one_led_V1.0/source/run_one_led.v
@@ -4,18 +4,17 @@ module run_one_led( CLOCK,  LED0);
 	//reg clk_1Hz;
 	reg LED0;
 	integer counter;
-		initial 
-		begin
-			counter = 0;
-			LED0 = 0;
-		end
+	initial 
+	begin
+		counter = 0;
+	end
 			
 	always @(negedge CLOCK)
 		begin
 			if( counter >= 50000000/2)
 				begin
 					LED0 <= !LED0;
-					counter = 0;
+					counter <= 0;
 				end
 			else 
 				counter <= (counter+1);			
